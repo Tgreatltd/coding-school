72b43920116c2663edb3c010e5fc99792ba7a16e
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-12-20 08:25:36 +0100

    msg

---
+++ src/school/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    <div> WELCOME TO OUR CODING SCHOOL</div>
+    <button className='btn btn-success'>login</button>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/school/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul style={{display:'flex', justifyContent:'space-evenly'}}>
+            <li>dashboard</li>
+            <li>profile</li>
+            <li>signup</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
