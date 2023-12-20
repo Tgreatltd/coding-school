441c26dabab0f189fa4a932bcc45daeb84df8f55
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-12-20 14:54:59 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 function App() {
   return (
     <div className="App">
--- src/school/Home.jsx
+++ src/school/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
 import './Coding.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHeart, faMugHot } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
+
   return (
     <div>
       <Navbar/>
     <div> WELCOME TO OUR CODING SCHOOL</div>
     <button className='btn btn-success'>login</button>
+    <FontAwesomeIcon icon={faEnvelope} />
+    <FontAwesomeIcon icon={faHeart} />
+    <FontAwesomeIcon icon={faMugHot} />
+
     </div>
   )
 }
