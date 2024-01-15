debe5cf2223b7ac804c4964582b5a657f0f0d6f1
oluwatobi <olamoyegunoluwagreat@gmail.com>
2024-01-15 17:46:16 +0100

    new commit

--- src/school/Home.jsx
+++ src/school/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
     <div>WHAT WE DO</div>
     
-    <div>WE TEACH PEOPLE HOW TO BE TECH ORIENTED</div> 
+    <div>WE TEACH PEOPLE HOW TO BE TECH ORIENTED</div>
     <button className='btn btn-success'>login</button>
     
     </div>
